97c66f40d29cae591593257b2d2c3e46ef7644ac
Pavel_Zhandin <pavelzhandin@gmail.com>
2023-01-01 23:19:28 +0300

    initial backend commit

---
+++ backend_sanity/schemas/testimonials.ts
@@ -0,0 +1,10 @@
+export default {
+  name: 'testimonials',
+  title: 'Testimonials',
+  type: 'document',
+  fields: [
+    {name: 'name', title: 'Name', type: 'string'},
+    {name: 'Company', title: 'Company', type: 'string'},
+    {name: 'name', title: 'Name', type: 'string'},
+  ],
+}
