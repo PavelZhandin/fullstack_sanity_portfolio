66138eae8d388f8816092740897dcc9b2335f895
Pavel_Zhandin <pavelzhandin@gmail.com>
2023-01-02 00:15:52 +0300

    add new schemas for backend, initialize base frontend template with ts

--- backend_sanity/schemas/index.ts
+++ backend_sanity/schemas/index.ts
@@ -1,1 +1,20 @@
-export const schemaTypes = []
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+import works from './works'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+  /* Your types here! */
+]
--- backend_sanity/schemas/testimonials.ts
+++ backend_sanity/schemas/testimonials.ts
@@ -3,8 +3,28 @@ export default {
   title: 'Testimonials',
   type: 'document',
   fields: [
-    {name: 'name', title: 'Name', type: 'string'},
-    {name: 'Company', title: 'Company', type: 'string'},
-    {name: 'name', title: 'Name', type: 'string'},
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'company',
+      title: 'Company',
+      type: 'string',
+    },
+    {
+      name: 'imgurl',
+      title: 'ImgUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'feedback',
+      title: 'Feedback',
+      type: 'string',
+    },
   ],
 }
